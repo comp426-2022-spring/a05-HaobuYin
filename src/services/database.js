66f5db850866da457252399376cf464114305d0c
Haobu Yin <haobu@haobu-pc.localdomain>
2022-05-02 00:16:46 -0400

    finished first two div

--- src/services/database.js
+++ src/services/database.js
@@ -1,1 +1,31 @@
 // Put your database code here
+const database = require('better-sqlite3')
+
+const logdb = new database('./data/db/log.db')
+
+const stmt = logdb.prepare(`SELECT name FROM sqlite_master WHERE type='table' and name='accesslog';`)
+let row = stmt.get();
+if (row === undefined) {
+    console.log('Log database appears to be empty. Creating log database...')
+    const sqlInit = `
+        CREATE TABLE accesslog ( 
+            id INTEGER PRIMARY KEY, 
+            remoteaddr TEXT, 
+            remoteuser TEXT, 
+            date TEXT, 
+            method TEXT, 
+            url TEXT, 
+            protocol TEXT,
+            httpversion NUMERIC, 
+            status INTEGER, 
+            content_length NUMERIC,
+            referrer_url TEXT,
+            user_agent TEXT
+        );
+    `
+    logdb.exec(sqlInit)
+} else {
+    console.log('Log database exists.')
+}
+
+module.exports = logdb
